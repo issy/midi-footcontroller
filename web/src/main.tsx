e17f45faab1017d6921e0b6bac872891b689c236
issy <issy@issy.dev>
2026-02-13 22:32:54 +0000

    Add Prettier

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,7 +1,7 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
 
 const rootElement = document.getElementById('root');
 
@@ -11,4 +11,4 @@ createRoot(rootElement).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);

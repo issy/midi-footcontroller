4ea65c06ab3b68c31ffc73e0b4f6aa008b31dac9
issy <issy@issy.dev>
2026-02-15 16:51:41 +0000

    Fix

--- web/src/views/NotFoundView.tsx
+++ web/src/views/NotFoundView.tsx
@@ -2,11 +2,11 @@ import { Link } from 'react-router';
 
 function NotFoundView() {
   return (
-    <alert>
+    <aside role="alert">
       <h2>Not found</h2>
       <p>The page you are looking for does not exist.</p>
       <Link to="/">Go back home</Link>
-    </alert>
+    </aside>
   );
 }
 

ee401017009a26e75d0d8cf9ffb12a8b0f3a4ab3
issy <issy@issy.dev>
2026-03-03 20:24:34 +0000

    Add rest of state (probably need to use mut refs, but that can come later)

--- firmware/foundation/src/application.rs
+++ firmware/foundation/src/application.rs
@@ -1,3 +1,4 @@
+use crate::midi::{MidiReader, MidiWriter};
 use embedded_graphics::draw_target::DrawTarget;
 
 struct Displays<D: DrawTarget> {
@@ -7,15 +8,27 @@ struct Displays<D: DrawTarget> {
     display_4: D,
 }
 
-pub struct Application<D: DrawTarget> {
+struct MidiStreams<MR: MidiReader, MW: MidiWriter> {
+    reader: MR,
+    writer: MW,
+}
+
+pub struct Application<D: DrawTarget, MR: MidiReader, MW: MidiWriter> {
     displays: Displays<D>,
-    // TODO: Add MIDI streams
+    midi_streams: MidiStreams<MR, MW>,
     // TODO: Add protocol streams
     // TODO: Add buttons
 }
 
-impl<D: DrawTarget> Application<D> {
-    pub fn new(display_1: D, display_2: D, display_3: D, display_4: D) -> Self {
+impl<D: DrawTarget, MR: MidiReader, MW: MidiWriter> Application<D, MR, MW> {
+    pub fn new(
+        display_1: D,
+        display_2: D,
+        display_3: D,
+        display_4: D,
+        midi_reader: MR,
+        midi_writer: MW,
+    ) -> Self {
         Self {
             displays: Displays {
                 display_1,
@@ -23,6 +36,68 @@ impl<D: DrawTarget> Application<D> {
                 display_3,
                 display_4,
             },
+            midi_streams: MidiStreams {
+                reader: midi_reader,
+                writer: midi_writer,
+            },
         }
+    }
+}
+
+struct ApplicationBuilder<D: DrawTarget, MR: MidiReader, MW: MidiWriter> {
+    display_1: Option<D>,
+    display_2: Option<D>,
+    display_3: Option<D>,
+    display_4: Option<D>,
+    midi_reader: Option<MR>,
+    midi_writer: Option<MW>,
+}
+
+impl<D: DrawTarget, MR: MidiReader, MW: MidiWriter> ApplicationBuilder<D, MR, MW> {
+    pub fn new() -> Self {
+        Self {
+            display_1: None,
+            display_2: None,
+            display_3: None,
+            display_4: None,
+            midi_reader: None,
+            midi_writer: None,
+        }
+    }
+
+    pub fn with_display(mut self, display: D) -> Self {
+        if self.display_1.is_none() {
+            self.display_1 = Some(display);
+        } else if self.display_2.is_none() {
+            self.display_2 = Some(display);
+        } else if self.display_3.is_none() {
+            self.display_3 = Some(display);
+        } else if self.display_4.is_none() {
+            self.display_4 = Some(display);
+        } else {
+            panic!("All 4 displays are already set");
+        }
+        self
+    }
+
+    pub fn with_midi_reader(mut self, reader: MR) -> Self {
+        self.midi_reader = Some(reader);
+        self
+    }
+
+    pub fn with_midi_writer(mut self, writer: MW) -> Self {
+        self.midi_writer = Some(writer);
+        self
+    }
+
+    pub fn build(self) -> Application<D, MR, MW> {
+        Application::new(
+            self.display_1.expect("Display 1 is required"),
+            self.display_2.expect("Display 2 is required"),
+            self.display_3.expect("Display 3 is required"),
+            self.display_4.expect("Display 4 is required"),
+            self.midi_reader.expect("MIDI reader is required"),
+            self.midi_writer.expect("MIDI writer is required"),
+        )
     }
 }

c5f7328fa5bdf42050c2884813caed11277f57bd
issy <issy@issy.dev>
2026-03-07 21:07:34 +0000

    Add channel to state

--- firmware/foundation/src/application/state.rs
+++ firmware/foundation/src/application/state.rs
@@ -1,12 +1,20 @@
 use crate::application::{Displays, MidiStreams};
-use crate::midi::{MidiReader, MidiWriter};
+use crate::midi::{MidiPacket, MidiReader, MidiWriter};
+use embassy_sync::blocking_mutex::raw::CriticalSectionRawMutex;
+use embassy_sync::channel::Channel;
 use embedded_graphics::draw_target::DrawTarget;
+
+type MidiOutChannel = Channel<CriticalSectionRawMutex, MidiPacket, 128>;
+
+struct InternalChannels<'a> {
+    midi_out: &'a mut MidiOutChannel,
+}
 
 pub struct Application<'a, D: DrawTarget, MR: MidiReader, MW: MidiWriter> {
     displays: Displays<'a, D>,
     midi_streams: MidiStreams<'a, MR, MW>,
-    // TODO: Add protocol streams
-    // TODO: Add buttons
+    channels: InternalChannels<'a>, // TODO: Add protocol streams
+                                    // TODO: Add buttons
 }
 
 impl<'a, D: DrawTarget, MR: MidiReader, MW: MidiWriter> Application<'a, D, MR, MW> {
@@ -17,6 +25,7 @@ impl<'a, D: DrawTarget, MR: MidiReader, MW: MidiWriter> Application<'a, D, MR, M
         display_4: &'a mut D,
         midi_reader: &'a mut MR,
         midi_writer: &'a mut MW,
+        midi_out_channel: &'a mut MidiOutChannel,
     ) -> Self {
         // Maybe a good idea to create the channels here?
         Self {
@@ -30,6 +39,9 @@ impl<'a, D: DrawTarget, MR: MidiReader, MW: MidiWriter> Application<'a, D, MR, M
                 reader: midi_reader,
                 writer: midi_writer,
             },
+            channels: InternalChannels {
+                midi_out: midi_out_channel,
+            },
         }
     }
 }
@@ -42,6 +54,7 @@ pub struct ApplicationBuilder<'a, D: DrawTarget, MR: MidiReader, MW: MidiWriter>
     display_4: Option<&'a mut D>,
     midi_reader: Option<&'a mut MR>,
     midi_writer: Option<&'a mut MW>,
+    midi_out_channel: Option<&'a mut MidiOutChannel>,
 }
 
 impl<'a, D: DrawTarget, MR: MidiReader, MW: MidiWriter> ApplicationBuilder<'a, D, MR, MW> {
@@ -53,6 +66,7 @@ impl<'a, D: DrawTarget, MR: MidiReader, MW: MidiWriter> ApplicationBuilder<'a, D
             display_4: None,
             midi_reader: None,
             midi_writer: None,
+            midi_out_channel: None,
         }
     }
 
@@ -81,6 +95,11 @@ impl<'a, D: DrawTarget, MR: MidiReader, MW: MidiWriter> ApplicationBuilder<'a, D
         self
     }
 
+    pub fn with_midi_out_channel(mut self, channel: &'a mut MidiOutChannel) -> Self {
+        self.midi_out_channel = Some(channel);
+        self
+    }
+
     pub fn build(self) -> Application<'a, D, MR, MW> {
         Application::new(
             self.display_1.expect("Display 1 is required"),
@@ -89,6 +108,7 @@ impl<'a, D: DrawTarget, MR: MidiReader, MW: MidiWriter> ApplicationBuilder<'a, D
             self.display_4.expect("Display 4 is required"),
             self.midi_reader.expect("MIDI reader is required"),
             self.midi_writer.expect("MIDI writer is required"),
+            self.midi_out_channel.expect("MIDI out channel is required"),
         )
     }
 }

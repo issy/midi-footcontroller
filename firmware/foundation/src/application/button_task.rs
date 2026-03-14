482fcedbe56689ebc186d08508c42b4e1d041cda
issy <issy@issy.dev>
2026-03-14 20:05:40 +0000

    Remove this

--- firmware/foundation/src/application/button_task.rs
+++ firmware/foundation/src/application/button_task.rs
@@ -1,12 +1,6 @@
 use crate::application::channels::ButtonEventChannel;
-use crate::application::state::Application;
-use crate::midi::{MidiReader, MidiWriter};
-use crate::storage::StorageManager;
-use embedded_graphics::draw_target::DrawTarget;
 
-pub async fn button_task<'a, D: DrawTarget, MR: MidiReader, MW: MidiWriter, SM: StorageManager>(
-    button_event_channel: ButtonEventChannel,
-) -> ! {
+pub async fn button_task(button_event_channel: ButtonEventChannel) -> ! {
     loop {
         let button_event = button_event_channel.receive().await;
     }

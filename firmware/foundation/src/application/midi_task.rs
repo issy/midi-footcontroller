7af86c67a830a6793e3d222474c1f9cb5d44ee29
issy <issy@issy.dev>
2026-03-15 16:04:46 +0000

    Add note

--- firmware/foundation/src/application/midi_task.rs
+++ firmware/foundation/src/application/midi_task.rs
@@ -4,6 +4,7 @@ use crate::midi::{MidiReader, MidiWriter};
 async fn midi_thru_task<MR: MidiReader>(mut reader: MR, midi_out_channel: MidiOutChannel) -> ! {
     loop {
         if let Some(packet) = reader.read_midi_packet().await.unwrap() {
+            // TODO: If we decide to support MIDI command input in future, this would be a good place to process those
             midi_out_channel.send(packet).await;
         }
     }

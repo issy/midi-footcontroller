a2377a5299bc589ef9a2c5d6b1dc33aa58d3643c
issy <issy@issy.dev>
2026-03-21 21:04:16 +0000

    Add postcss config for mantine

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
 import App from './App';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { BrowserRouter } from 'react-router';
 import { MantineProvider } from '@mantine/core';
+import './index.css';
+import '@mantine/core/styles.css';
 
 const queryClient = new QueryClient();
 

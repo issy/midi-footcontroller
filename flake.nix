188a625fb577b5a7149c0238bb92e2d48a565c91
issy <issy@issy.dev>
2026-03-21 17:06:39 +0000

    Add sub packages

--- flake.nix
+++ flake.nix
@@ -2,8 +2,19 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     utils.url = "github:numtide/flake-utils";
+
+    app-firmware = {
+      url = "./firmware";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.utils.follows = "utils";
+    };
+    app-web = {
+      url = "./web";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.utils.follows = "utils";
+    };
   };
-  outputs = { self, nixpkgs, utils }: utils.lib.eachDefaultSystem (system:
+  outputs = { self, nixpkgs, utils, app-firmware, app-web }: utils.lib.eachDefaultSystem (system:
     let
       pkgs = nixpkgs.legacyPackages.${system};
     in

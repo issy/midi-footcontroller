d983356a70198bd42a0d5f1b7be863a14f7facdd
issy <issy@issy.dev>
2026-03-21 19:58:35 +0000

    Refactor

--- web/src/views/device/DeviceLayout.tsx
+++ web/src/views/device/DeviceLayout.tsx
@@ -1,5 +1,5 @@
 import { Outlet } from 'react-router';
-import { useCallback, useEffect, useState } from 'react';
+import { ConnectionManagerProvider, useConnectionManager } from '@/views/device/connection-manager';
 
 function BrowserNotSupported() {
   return (
@@ -10,65 +10,19 @@ function BrowserNotSupported() {
   );
 }
 
-type ConnectionManager =
-  | {
-      isConnected: false;
-      connect(): Promise<void>;
-    }
-  | {
-      isConnected: true;
-      disconnect(): Promise<void>;
-    };
-
-function useConnectionManager(): ConnectionManager {
-  const [isConnected, setIsConnected] = useState(false);
-
-  const connect = useCallback(async () => {
-    const port = await navigator.serial.requestPort();
-    await port.open({ baudRate: 115_200 });
-  }, []);
-
-  const disconnect = useCallback(async () => {
-    const ports = await navigator.serial.getPorts();
-    for (const port of ports) {
-      await port.close();
-    }
-  }, []);
-
-  useEffect(() => {
-    function onConnectListener() {
-      setIsConnected(true);
-    }
-
-    navigator.serial.addEventListener('connect', onConnectListener);
-    return () => {
-      navigator.serial.removeEventListener('connect', onConnectListener);
-    };
-  }, []);
-
-  useEffect(() => {
-    function onDisconnectListener() {
-      setIsConnected(false);
-    }
-
-    navigator.serial.addEventListener('disconnect', onDisconnectListener);
-    return () => {
-      navigator.serial.removeEventListener('disconnect', onDisconnectListener);
-    };
-  }, []);
-
-  return isConnected ? { isConnected: true, disconnect } : { isConnected: false, connect };
-}
-
 function DeviceLayout() {
-  // TODO: Use the device here and pass it down to the children through context
-  useConnectionManager();
+  // TODO: Wrap the connection manager in a device wrapper
+  const connectionManager = useConnectionManager();
 
   if (!('serial' in navigator)) {
     return <BrowserNotSupported />;
   }
 
-  return <Outlet />;
+  return (
+    <ConnectionManagerProvider value={connectionManager}>
+      <Outlet />
+    </ConnectionManagerProvider>
+  );
 }
 
 export default DeviceLayout;
---
+++ web/src/views/device/connection-manager.ts
@@ -0,0 +1,63 @@
+import { createContext, use, useCallback, useEffect, useState } from 'react';
+
+type ConnectionManager =
+  | {
+      isConnected: false;
+      connect(): Promise<void>;
+    }
+  | {
+      isConnected: true;
+      disconnect(): Promise<void>;
+    };
+
+const ConnectionManagerContext = createContext<ConnectionManager | undefined>(undefined);
+ConnectionManagerContext.displayName = 'ConnectionManagerContext';
+export const ConnectionManagerProvider = ConnectionManagerContext.Provider;
+
+export const useConnectionManagerContext = () => {
+  const context = use(ConnectionManagerContext);
+  if (context === undefined) {
+    throw new Error('useConnectionManagerContext must be used within a ConnectionManagerProvider');
+  }
+  return context;
+};
+
+export const useConnectionManager = (): ConnectionManager => {
+  const [isConnected, setIsConnected] = useState(false);
+
+  const connect = useCallback(async () => {
+    const port = await navigator.serial.requestPort();
+    await port.open({ baudRate: 31_250 });
+  }, []);
+
+  const disconnect = useCallback(async () => {
+    const ports = await navigator.serial.getPorts();
+    for (const port of ports) {
+      await port.close();
+    }
+  }, []);
+
+  useEffect(() => {
+    function onConnectListener() {
+      setIsConnected(true);
+    }
+
+    navigator.serial.addEventListener('connect', onConnectListener);
+    return () => {
+      navigator.serial.removeEventListener('connect', onConnectListener);
+    };
+  }, []);
+
+  useEffect(() => {
+    function onDisconnectListener() {
+      setIsConnected(false);
+    }
+
+    navigator.serial.addEventListener('disconnect', onDisconnectListener);
+    return () => {
+      navigator.serial.removeEventListener('disconnect', onDisconnectListener);
+    };
+  }, []);
+
+  return isConnected ? { isConnected: true, disconnect } : { isConnected: false, connect };
+};

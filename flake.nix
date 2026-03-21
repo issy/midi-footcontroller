c3c6d31d07f738ae200d1bb76c0f3c15c58f0939
issy <issy@issy.dev>
2026-03-21 17:08:41 +0000

    Fix utils reference

--- flake.nix
+++ flake.nix
@@ -1,20 +1,20 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
-    utils.url = "github:numtide/flake-utils";
+    flake-utils.url = "github:numtide/flake-utils";
 
     app-firmware = {
       url = "./firmware";
       inputs.nixpkgs.follows = "nixpkgs";
-      inputs.utils.follows = "utils";
+      inputs.flake-utils.follows = "flake-utils";
     };
     app-web = {
       url = "./web";
       inputs.nixpkgs.follows = "nixpkgs";
-      inputs.utils.follows = "utils";
+      inputs.flake-utils.follows = "flake-utils";
     };
   };
-  outputs = { self, nixpkgs, utils, app-firmware, app-web }: utils.lib.eachDefaultSystem (system:
+  outputs = { self, nixpkgs, flake-utils, app-firmware, app-web }: flake-utils.lib.eachDefaultSystem (system:
     let
       pkgs = nixpkgs.legacyPackages.${system};
     in

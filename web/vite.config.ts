f6b1f43c7321a3e721b01c266ad9dcedd9cb8cf7
issy <issy@issy.dev>
2026-03-22 20:00:58 +0000

    Remove tailwindcss plugin

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,5 +1,4 @@
 import { defineConfig } from 'vite';
-import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
@@ -10,7 +9,6 @@ export default defineConfig({
         plugins: [['babel-plugin-react-compiler']],
       },
     }),
-    tailwindcss(),
   ],
   resolve: {
     alias: {

a4109fa10a0cbc9d9a68931264184ea0b86e76ba
issy <issy@issy.dev>
2026-03-22 21:10:58 +0000

    Add colour scheme switcher button

--- web/src/views/AppLayout.tsx
+++ web/src/views/AppLayout.tsx
@@ -1,10 +1,34 @@
 import { Outlet } from 'react-router';
-import { AppShell } from '@mantine/core';
+import { ActionIcon, AppShell, Group, useComputedColorScheme, useMantineColorScheme } from '@mantine/core';
+import { MdDarkMode, MdLightMode } from 'react-icons/md';
+
+function ColourSchemeButton() {
+  const { setColorScheme } = useMantineColorScheme();
+  const computedColorScheme = useComputedColorScheme(undefined, { getInitialValueInEffect: true });
+
+  return (
+    <Group justify="center">
+      <ActionIcon
+        onClick={() => {
+          setColorScheme(computedColorScheme === 'light' ? 'dark' : 'light');
+        }}
+        variant="default"
+        size="xl"
+        radius="md"
+        aria-label="Toggle color scheme"
+      >
+        {computedColorScheme === 'light' && <MdDarkMode />}
+        {computedColorScheme === 'dark' && <MdLightMode />}
+      </ActionIcon>
+    </Group>
+  );
+}
 
 function AppLayout() {
   return (
     <AppShell padding="md">
       <AppShell.Main>
+        <ColourSchemeButton />
         <Outlet />
       </AppShell.Main>
     </AppShell>

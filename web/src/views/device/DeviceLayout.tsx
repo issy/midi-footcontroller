6ca5d1fe01d3790a596f363d81a4948fc22e8b37
issy <issy@issy.dev>
2026-03-23 20:53:40 +0000

    Add dropdown for connection

--- web/src/views/device/DeviceLayout.tsx
+++ web/src/views/device/DeviceLayout.tsx
@@ -1,15 +1,48 @@
 import { Outlet } from 'react-router';
 import ConnectionManagerProvider from '@/views/device/connection-manager/ConnectionManagerProvider';
-import { AppShell, Button, Group, Text } from '@mantine/core';
+import { ActionIcon, AppShell, Button, Group, Menu, Stack, Text } from '@mantine/core';
 import ColourSchemeButton from '@/components/ColourSchemeButton';
 import { useConnectionManagerContext } from '@/views/device/connection-manager/context';
-import { MdOutlineUsb, MdOutlineUsbOff } from 'react-icons/md';
+import { MdOutlineUsb, MdOutlineUsbOff, MdArrowDropDown, MdCode } from 'react-icons/md';
+import classes from './DeviceLayout.module.scss';
+
+function DropdownButton() {
+  return (
+    <Group wrap="nowrap" gap={0}>
+      <Button leftSection={<MdOutlineUsb />} className={classes.connectDropdownButton}>
+        <Text>Connect</Text>
+      </Button>
+      <Menu transitionProps={{ transition: 'pop' }} position="bottom-end" withinPortal>
+        <Menu.Target>
+          <ActionIcon variant="filled" size={36} className={classes.connectDropdownIcon}>
+            <MdArrowDropDown />
+          </ActionIcon>
+        </Menu.Target>
+        <Menu.Dropdown>
+          <Menu.Item
+            leftSection={<MdCode />}
+            onClick={() => {
+              // TODO: Actually start and connect the simulator here
+              console.log('Connect to simulator');
+            }}
+          >
+            <Text>Connect to simulator</Text>
+          </Menu.Item>
+        </Menu.Dropdown>
+      </Menu>
+    </Group>
+  );
+}
 
 function BrowserNotSupported() {
   return (
     <aside role="alert">
-      <h3>Your browser is not supported</h3>
-      <p>Please use a browser with Web Serial support (such as Chrome) in order to connect to your device</p>
+      <Stack>
+        <Text size="lg" component="h3">
+          Your browser is not supported
+        </Text>
+        <Text>Please use a browser with Web Serial support (such as Chrome) in order to connect to your device</Text>
+      </Stack>
     </aside>
   );
 }
@@ -41,6 +74,7 @@ function DeviceLayout() {
           <Group>
             {/* TODO: Update button */}
             {/* TODO: Connection menu button */}
+            <DropdownButton />
             <ConnectionStatus />
             <ColourSchemeButton />
           </Group>

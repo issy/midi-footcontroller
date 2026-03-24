dcfe6795c7845a7d43fc1985d80a537e9245e515
issy <issy@issy.dev>
2026-03-24 18:11:30 +0000

    Remove original connect button

--- web/src/views/device/DeviceConnectView.tsx
+++ web/src/views/device/DeviceConnectView.tsx
@@ -1,48 +1,24 @@
-import { MdOutlineUsb, MdOutlineUsbOff } from 'react-icons/md';
-import { type ConnectionManager, useConnectionManagerContext } from '@/views/device/connection-manager/context';
-import { Button, Text } from '@mantine/core';
-import { Fragment } from 'react';
-import { useMutation } from '@tanstack/react-query';
-
-const DisconnectedView = ({ connect }: Pick<Extract<ConnectionManager, { isConnected: false }>, 'connect'>) => {
-  const { isPending, mutate } = useMutation({
-    mutationFn: connect,
-    mutationKey: ['connect'],
-  });
-
-  return (
-    <Fragment>
-      <h3 style={{ display: 'flex' }}>
-        <MdOutlineUsbOff />
-        <Text>Disconnected</Text>
-      </h3>
-      <Button
-        leftSection={<MdOutlineUsb />}
-        loading={isPending}
-        onClick={() => {
-          mutate();
-        }}
-      >
-        <Text>Connect</Text>
-      </Button>
-    </Fragment>
-  );
-};
+import { useConnectionManagerContext } from '@/views/device/connection-manager/context';
+import { AppShell, Text } from '@mantine/core';
 
 // TODO: This should just show a big error state that the device is not connected
 function DeviceConnectView() {
   const conn = useConnectionManagerContext();
 
   return (
-    <div>
-      {conn.isConnected ? (
-        <div>
-          <Text>Connected</Text>
-        </div>
-      ) : (
-        <DisconnectedView connect={conn.connect} />
-      )}
-    </div>
+    <AppShell.Main>
+      <div>
+        {conn.isConnected ? (
+          <div>
+            <Text>Connected</Text>
+          </div>
+        ) : (
+          <div>
+            <Text>Disconnected</Text>
+          </div>
+        )}
+      </div>
+    </AppShell.Main>
   );
 }
 

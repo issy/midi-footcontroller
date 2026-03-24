14c90cbd5cf651d50f7897cdec8f9027ca62da04
issy <issy@issy.dev>
2026-03-24 20:24:07 +0000

    Extract mock data

--- web/src/views/device/editor/DevicePresetView.tsx
+++ web/src/views/device/editor/DevicePresetView.tsx
@@ -1,32 +1,96 @@
 import { AppShell, AspectRatio, Button, type MantineColor, SimpleGrid, Stack, Title } from '@mantine/core';
 import { useParams } from 'react-router';
+import { useState } from 'react';
 
-function DeviceButton({ color, text }: { color: MantineColor; text: string }) {
+function DeviceButton({
+  color,
+  text,
+  active,
+  onClick,
+}: {
+  color: MantineColor;
+  text: string;
+  active: boolean;
+  onClick: () => void;
+}) {
   return (
     <AspectRatio ratio={3}>
-      <Button color={color} fullWidth h="100%">
+      <Button variant={active ? 'filled' : 'light'} color={color} fullWidth h="100%" onClick={onClick}>
         <Title>{text}</Title>
       </Button>
     </AspectRatio>
   );
 }
 
+interface PresetData {
+  color: MantineColor;
+  text: string;
+  id: string;
+}
+
+const mockPresetData: Array<PresetData> = [
+  {
+    id: 'one',
+    color: 'red',
+    text: 'Hello',
+  },
+  {
+    id: 'two',
+    color: 'green',
+    text: 'Hello',
+  },
+  {
+    id: 'three',
+    color: 'blue',
+    text: 'Hello',
+  },
+  {
+    id: 'four',
+    color: 'yellow',
+    text: 'Hello',
+  },
+  {
+    id: 'five',
+    color: 'green',
+    text: 'Hello',
+  },
+  {
+    id: 'six',
+    color: 'grape',
+    text: 'Hello',
+  },
+  {
+    id: 'seven',
+    color: 'teal',
+    text: 'Hello',
+  },
+  {
+    id: 'eight',
+    color: 'blue',
+    text: 'Hello',
+  },
+];
+
 function DevicePresetView() {
   const { presetId } = useParams<{ presetId: string }>();
+  const [active, setActive] = useState<string | undefined>(undefined);
 
   return (
     <AppShell.Main>
       <Stack gap="xs">
         <Title>{presetId}</Title>
         <SimpleGrid cols={4} spacing="md">
-          <DeviceButton color="red" text="Hello" />
-          <DeviceButton color="blue" text="Hello" />
-          <DeviceButton color="green" text="Hello" />
-          <DeviceButton color="yellow" text="Hello" />
-          <DeviceButton color="grape" text="Hello" />
-          <DeviceButton color="green" text="Hello" />
-          <DeviceButton color="blue" text="Hello" />
-          <DeviceButton color="teal" text="Hello" />
+          {mockPresetData.map((preset) => (
+            <DeviceButton
+              key={`button_${preset.id}`}
+              color={preset.color}
+              text={preset.text}
+              active={active === preset.id}
+              onClick={() => {
+                setActive(preset.id);
+              }}
+            />
+          ))}
         </SimpleGrid>
       </Stack>
     </AppShell.Main>

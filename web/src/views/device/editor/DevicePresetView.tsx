e383f325e16727ea9eecc03b53f30c9b6b8841b7
issy <issy@issy.dev>
2026-03-24 20:07:20 +0000

    Button layout

--- web/src/views/device/editor/DevicePresetView.tsx
+++ web/src/views/device/editor/DevicePresetView.tsx
@@ -1,15 +1,32 @@
-import { AppShell } from '@mantine/core';
+import { AppShell, AspectRatio, Button, type MantineColor, SimpleGrid, Title } from '@mantine/core';
 import { useParams } from 'react-router';
+
+function DeviceButton({ color, text }: { color: MantineColor; text: string }) {
+  return (
+    <AspectRatio ratio={3}>
+      <Button color={color} fullWidth h="100%">
+        <Title>{text}</Title>
+      </Button>
+    </AspectRatio>
+  );
+}
 
 function DevicePresetView() {
   const { presetId } = useParams<{ presetId: string }>();
 
   return (
     <AppShell.Main>
-      <div>
-        <p>foobar</p>
-        <p>{presetId}</p>
-      </div>
+      <SimpleGrid cols={4} spacing="md">
+        <DeviceButton color="red" text="Hello" />
+        <DeviceButton color="blue" text="Hello" />
+        <DeviceButton color="green" text="Hello" />
+        <DeviceButton color="yellow" text="Hello" />
+        <DeviceButton color="grape" text="Hello" />
+        <DeviceButton color="green" text="Hello" />
+        <DeviceButton color="blue" text="Hello" />
+        <DeviceButton color="teal" text="Hello" />
+      </SimpleGrid>
+      {presetId}
     </AppShell.Main>
   );
 }

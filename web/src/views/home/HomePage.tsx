51a4edb453251ffd962c728885a32485739c078a
issy <issy@issy.dev>
2026-04-03 22:50:31 +0100

    Add CTA

--- web/src/views/home/HomePage.tsx
+++ web/src/views/home/HomePage.tsx
@@ -1,17 +1,36 @@
 import classes from './HomePage.module.scss';
-import { Container, Flex, Text } from '@mantine/core';
+import { Button, Container, Flex, Stack, Text } from '@mantine/core';
+import { FaGithub } from 'react-icons/fa6';
+import { MdOpenInNew } from 'react-icons/md';
 
 export function HomePage() {
   return (
     <div className={classes.container}>
       <Container size="xl">
         <Flex justify="space-between" gap="lg">
-          <div className={classes.heroSection}>
+          <Stack className={classes.heroSection}>
             <h1 className={classes.title}>MIDI-X</h1>
             <Text c="dimmed" size="xl">
               A completely configurable MIDI controller built for the stage
             </Text>
-          </div>
+            <Flex gap="md">
+              <Button size="lg" variant="gradient" gradient={{ from: 'indigo', to: 'blueviolet', deg: 20 }}>
+                Learn more
+              </Button>
+              <Button
+                size="lg"
+                variant="light"
+                color="gray"
+                leftSection={<FaGithub size={28} />}
+                rightSection={<MdOpenInNew />}
+                component="a"
+                href="https://github.com/issy/midi-footcontroller"
+                target="_blank"
+              >
+                GitHub
+              </Button>
+            </Flex>
+          </Stack>
         </Flex>
       </Container>
     </div>

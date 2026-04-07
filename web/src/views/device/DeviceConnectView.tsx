1390d871c4b267a7d728c85bbcda5a82a336af99
issy <issy@issy.dev>
2026-04-07 22:09:35 +0100

    Made the text red

--- web/src/views/device/DeviceConnectView.tsx
+++ web/src/views/device/DeviceConnectView.tsx
@@ -1,22 +1,33 @@
 import { useConnectionManagerContext } from '@/views/device/connection-manager/context';
-import { AppShell, Text } from '@mantine/core';
+import { AppShell, Container, Stack, Text, Title } from '@mantine/core';
+import classes from './DeviceConnectView.module.scss';
+import { MdOutlineUsbOff } from 'react-icons/md';
 
 // TODO: This should just show a big error state that the device is not connected
 function DeviceConnectView() {
   const conn = useConnectionManagerContext();
 
+  if (!conn.isConnected) {
+    return (
+      <div className={classes.container}>
+        <Container size="xl">
+          <Stack>
+            <div className={classes.containerInner}>
+              <MdOutlineUsbOff size={60} className={classes.disconnectedIcon} />
+              <Title className={classes.disconnectedText}>Disconnected</Title>
+            </div>
+          </Stack>
+        </Container>
+      </div>
+    );
+  }
+
   return (
     <AppShell.Main>
       <div>
-        {conn.isConnected ? (
-          <div>
-            <Text>Connected</Text>
-          </div>
-        ) : (
-          <div>
-            <Text>Disconnected</Text>
-          </div>
-        )}
+        <div>
+          <Text>Connected</Text>
+        </div>
       </div>
     </AppShell.Main>
   );

7aa9e951279d04652a1299c97ee04b681918c6c5
issy <issy@issy.dev>
2026-04-10 21:38:21 +0100

    Add initial button action edit form

--- web/src/views/device/editor/ButtonActionEditForm.tsx
+++ web/src/views/device/editor/ButtonActionEditForm.tsx
@@ -1,45 +1,100 @@
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { z } from 'zod';
+import FormField from '@/components/FormField';
+import { Button } from '@mantine/core';
 
-const ProgramChangeSchema = z.object({
+// TODO: Should these be zero-indexed?
+const midiChannelNumberSchema = z.int().min(1).max(16);
+const midiValueNumberSchema = z.int().min(1).max(128);
+
+const programChangeSchema = z.object({
   type: z.literal('PROGRAM_CHANGE'),
-  channel: z.number(),
-  program: z.number(),
+  channel: midiChannelNumberSchema,
+  program: midiValueNumberSchema,
 });
 
-const ControlChangeSchema = z.object({
+const controlChangeSchema = z.object({
   type: z.literal('CONTROL_CHANGE'),
-  channel: z.number(),
-  control: z.number(),
-  value: z.number(),
+  channel: midiChannelNumberSchema,
+  control: midiValueNumberSchema,
+  value: midiValueNumberSchema,
 });
 
-const NoteOnSchema = z.object({
+const noteOnSchema = z.object({
   type: z.literal('NOTE_ON'),
-  channel: z.number(),
+  channel: midiChannelNumberSchema,
   note: z.number(),
-  velocity: z.number(),
+  velocity: midiValueNumberSchema,
 });
 
-const NoteOffSchema = z.object({
+const noteOffSchema = z.object({
   type: z.literal('NOTE_OFF'),
-  channel: z.number(),
+  channel: midiChannelNumberSchema,
   note: z.number(),
 });
 
-const ButtonActionSchema = () =>
-  z.discriminatedUnion('type', [ProgramChangeSchema, ControlChangeSchema, NoteOnSchema, NoteOffSchema]);
-const schema = ButtonActionSchema();
+const buttonActionSchema = () =>
+  z.discriminatedUnion('type', [programChangeSchema, controlChangeSchema, noteOnSchema, noteOffSchema]);
+const schema = buttonActionSchema();
 
-type FormValues = z.infer<ReturnType<typeof ButtonActionSchema>>;
+type FormValues = z.infer<ReturnType<typeof buttonActionSchema>>;
 
 function ButtonActionEditForm() {
-  useForm<FormValues>({
+  const { handleSubmit, control } = useForm<FormValues>({
     resolver: zodResolver(schema),
+    defaultValues: {
+      type: 'CONTROL_CHANGE',
+    },
   });
 
-  return <div>Form will go here</div>;
+  const submitHandler = (data: FormValues) => {
+    console.log('submitHandler', data);
+  };
+
+  return (
+    <form
+      onSubmit={(e) => {
+        void handleSubmit(submitHandler)(e);
+      }}
+    >
+      <FormField
+        control={control}
+        projection={{
+          fieldName: 'type',
+          type: 'select',
+          label: 'Type',
+          options: [{ value: 'CONTROL_CHANGE', label: 'CC Message' }],
+        }}
+      />
+      <FormField
+        control={control}
+        projection={{
+          fieldName: 'channel',
+          type: 'number',
+          label: 'Channel',
+          placeholder: 'Select a MIDI channel',
+        }}
+      />
+      <FormField
+        control={control}
+        projection={{
+          fieldName: 'control',
+          type: 'number',
+          label: 'Control',
+        }}
+      />
+      <FormField
+        control={control}
+        projection={{
+          fieldName: 'value',
+          type: 'number',
+          label: 'Value',
+        }}
+      />
+      <Button type="submit">Submit</Button>
+    </form>
+  );
 }
 
 export default ButtonActionEditForm;

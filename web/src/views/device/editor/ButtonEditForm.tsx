6aa353c2fb7826489a2ee04bed0f343a361fe77f
issy <issy@issy.dev>
2026-04-12 13:19:32 +0100

    This is working, but will need reimplementing

--- web/src/views/device/editor/ButtonEditForm.tsx
+++ web/src/views/device/editor/ButtonEditForm.tsx
@@ -1,13 +1,30 @@
 import { z } from 'zod';
-import { useForm } from 'react-hook-form';
+import { Controller, useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { Button, Stack } from '@mantine/core';
+import { Button, ColorInput, Stack } from '@mantine/core';
 import FormField from '@/components/FormField';
+
+const hexToColour = {
+  '#fa5252': 'RED',
+  '#34d399': 'GREEN',
+  '#2563eb': 'BLUE',
+  '#fbbf24': 'YELLOW',
+  '#f97316': 'ORANGE',
+  '#8b5cf6': 'PURPLE',
+  '#06b6d4': 'CYAN',
+  '#ffffff': 'WHITE',
+} as const;
+
+const colourToHex = Object.fromEntries(Object.entries(hexToColour).map(([key, value]) => [value, key])) as Record<
+  (typeof hexToColour)[keyof typeof hexToColour],
+  keyof typeof hexToColour
+>;
 
 const buttonSchema = z.object({
   // TODO: Is this the same length defined in the firmware crate?
   // TODO: Refine this to alphanumeric characters only?
   name: z.string().max(16),
+  colour: z.enum(['RED', 'GREEN', 'BLUE', 'YELLOW', 'ORANGE', 'PURPLE', 'CYAN', 'WHITE']),
 });
 
 type FormValues = z.infer<typeof buttonSchema>;
@@ -41,6 +58,27 @@ function ButtonEditForm({ initialValues, onSubmit }: ButtonEditFormProps) {
             fieldName: 'name',
             type: 'text',
             label: 'Name',
+          }}
+        />
+        <Controller
+          name="colour"
+          control={control}
+          render={({ field, fieldState }) => {
+            const error = fieldState.error?.message;
+            const valueAsHex = colourToHex[field.value];
+            return (
+              <ColorInput
+                value={valueAsHex}
+                onChange={(hex) => {
+                  field.onChange(hexToColour[hex as keyof typeof hexToColour]);
+                }}
+                swatches={Object.keys(hexToColour)}
+                error={error}
+                format="hex"
+                withEyeDropper={false}
+                withPicker={false}
+              />
+            );
           }}
         />
         <Button type="submit" disabled={!isDirty} loading={isSubmitting}>
